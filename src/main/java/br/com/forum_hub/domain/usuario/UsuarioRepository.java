541f54106f66de0fb135cdd5dae4544ca6f124e1
iasminaraujoc <iasmincorreaaraujo@gmail.com>
2024-10-24 14:25:48 -0300

    video 4.4

--- src/main/java/br/com/forum_hub/domain/usuario/UsuarioRepository.java
+++ src/main/java/br/com/forum_hub/domain/usuario/UsuarioRepository.java
@@ -6,4 +6,6 @@ import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByEmailIgnoreCaseAndVerificadoTrue(String email);
+
+    Optional<Usuario> findByToken(String codigo);
 }
