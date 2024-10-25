2c15eabdd040e798f5ac239c1713cacadac2d35e
iasminaraujoc <iasmincorreaaraujo@gmail.com>
2024-10-25 11:26:48 -0300

    video 5.1

--- src/main/java/br/com/forum_hub/domain/usuario/Usuario.java
+++ src/main/java/br/com/forum_hub/domain/usuario/Usuario.java
@@ -1,4 +1,6 @@
 package br.com.forum_hub.domain.usuario;
+import br.com.forum_hub.domain.perfil.Perfil;
+import br.com.forum_hub.domain.perfil.PerfilNome;
 import br.com.forum_hub.infra.exception.RegraDeNegocioException;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -29,6 +32,7 @@ public class Usuario implements UserDetails {
     private String token;
     private LocalDateTime expiracaoToken;
     private Boolean ativo;
+    private List<Perfil> perfis;
 
     @Deprecated
     public Usuario(){}
